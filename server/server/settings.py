0177507bbcbede9750a0ff6240ef4918d1b01987
Loukas <kwstarikasloukas@gmail.com>
2026-01-30 00:34:25 +0200

    Add def-spectacular for exposing swagger endpoints

--- server/server/settings.py
+++ server/server/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_spectacular',
 
     'authentication',
 ]
@@ -139,4 +140,13 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+# Swagger / OpenAPI
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Fair Exchange Dice API',
+    'DESCRIPTION': 'API for the Fair Exchange Dice application',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
 }
